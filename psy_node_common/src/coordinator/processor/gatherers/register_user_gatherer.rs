b8202a80740ca937ec458e788938c72a8518e441
zilong-dai <2840478265@qq.com>
2025-12-22 17:48:42 +0800

    save public_key_hash to user_id map instead of registration id

--- psy_node_common/src/coordinator/processor/gatherers/register_user_gatherer.rs
+++ psy_node_common/src/coordinator/processor/gatherers/register_user_gatherer.rs
@@ -15,7 +15,7 @@ use parth_core::{
     protocol::core_types::{Q256BitHash, QDBHashBase, QNetworkTypesConfig},
     QCoreProcCheckpointUniqueId,
 };
-use psy_core::job::job_id::{ProvingJobCircuitType, QProvingJobDataID};
+use psy_core::{job::job_id::{ProvingJobCircuitType, QProvingJobDataID}, user_id::get_user_id_from_registration_id};
 use psy_data::{
     agg::{
         tree_agg_v2::{plan_jobs_for_tree_agg_offset_root, BasicTreePlannerHelper},
@@ -349,7 +349,12 @@ impl<
         let hash = hash_two_from_slice::<N::QHash, N::HasherBase>(&item);
         let u64_hash_mapping_row = QHash256AndU64 {
             hash,
-            value_u64: self.next_user_id,
+            value_u64: get_user_id_from_registration_id(
+                self.next_user_id,
+                N::COORDINATOR_GLOBAL_USER_TREE_HEIGHT,
+                N::REALM_GLOBAL_USER_TREE_HEIGHT,
+                N::GROUP_REALM_HEIGHT,
+            ),
         };
         self.new_public_key_hash_to_user_id_rows_ffs
             .extend_from_slice(&u64_hash_mapping_row.ffs_to_bytes());
