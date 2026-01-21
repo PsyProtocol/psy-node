b1d41ab54ef57eabb6259cc140bd2e3488a6e587
0x8f701 <alex_cj96@foxmail.com>
2026-01-22 00:41:20 +0800

    switch to random url rotation strategy
    Signed-off-by: 0x8f701 <alex_cj96@foxmail.com>

--- psy_cli/psy_worker_cli/src/subcommand.rs
+++ psy_cli/psy_worker_cli/src/subcommand.rs
@@ -45,7 +45,7 @@ pub enum Commands {
         #[arg(long = "realm-api-url", help = "Realm Edge API URLs for the worker to connect to (supports many)")]
         realm_api_urls: Vec<String>,
 
-        #[arg(long = "url-rotation-strategy", default_value = "continue-until-failure", help = "URL rotation strategy (round-robin, random, continue-until-failure, continue-until-failure-or-no-work-for-3-seconds, smart-swap-v1)")]
+        #[arg(long = "url-rotation-strategy", default_value = "random", help = "URL rotation strategy (round-robin, random, continue-until-failure, continue-until-failure-or-no-work-for-3-seconds, smart-swap-v1)")]
         url_rotation_strategy: PsyAPIURLRotationStrategyInput,
 
         #[arg(long = "batch-size", default_value = "4", help = "Number of jobs to fetch and process concurrently")]
