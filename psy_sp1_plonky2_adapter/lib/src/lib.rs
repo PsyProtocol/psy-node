d9a4558a374c557cd410be28743b1af9367e028a
PsyProtocol <team@psy.xyz>
2026-06-12 09:55:37 +0800

    chore: remove unused dirs (psy_common, psy_type_defaults, psy_sp1_plonky2_adapter, psy_scratchpad, psy_plonky2_testbed)

--- psy_sp1_plonky2_adapter/lib/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! SP1 Plonky2 Adapter Library
-//!
-//! This library contains shared types and utilities for the SP1-based
-//! Plonky2 to Groth16 proof compression.
-
-pub mod types;
-
-pub use types::*;
--- psy_sp1_plonky2_adapter/program/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod verifier;
-
-pub use verifier::{compute_public_inputs_hash, split_hash_to_bn254, ProgramError};
