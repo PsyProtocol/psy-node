89932cd12124b34da04d8f871cd7c6ed55823226
hellojason3 <hellojason3@gmail.com>
2026-08-28 17:09:26 +0800

    fix(deploy): pass psy-services Nostr relay URL

--- deploy/gcp/lib/psy-services-nostr.sh
+++ deploy/gcp/lib/psy-services-nostr.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 resolve_psy_services_nostr_config() {
-  PSY_NOSTR_RELAY_URLS="${PSY_NOSTR_RELAY_URLS:-${NOSTR_RELAY_URL:-}}"
+  if [ -z "${PSY_NOSTR_RELAY_URL:-}" ]; then
+    local configured_relays="${PSY_NOSTR_RELAY_URLS:-${NOSTR_RELAY_URL:-}}"
+    PSY_NOSTR_RELAY_URL="${configured_relays%%,*}"
+  fi
+  PSY_NOSTR_RELAY_URLS="${PSY_NOSTR_RELAY_URLS:-$PSY_NOSTR_RELAY_URL}"
   PSY_NOSTR_LOOKBACK_SECONDS="${PSY_NOSTR_LOOKBACK_SECONDS:-259200}"
 
   if [ -z "${PSY_NOSTR_ENABLED:-}" ]; then
-    if [ -n "$PSY_NOSTR_RELAY_URLS" ]; then
+    if [ -n "$PSY_NOSTR_RELAY_URL" ]; then
       PSY_NOSTR_ENABLED="1"
     else
       PSY_NOSTR_ENABLED="0"
@@ -15,8 +19,8 @@ resolve_psy_services_nostr_config() {
   case "${PSY_NOSTR_ENABLED,,}" in
     1|true|yes|on)
       PSY_NOSTR_ENABLED="1"
-      if [ -z "$PSY_NOSTR_RELAY_URLS" ]; then
-        echo "PSY_NOSTR_RELAY_URLS is required when PSY_NOSTR_ENABLED=1" >&2
+      if [ -z "$PSY_NOSTR_RELAY_URL" ]; then
+        echo "PSY_NOSTR_RELAY_URL is required when PSY_NOSTR_ENABLED=1" >&2
         return 1
       fi
       ;;
@@ -34,5 +38,5 @@ resolve_psy_services_nostr_config() {
     return 1
   fi
 
-  export PSY_NOSTR_ENABLED PSY_NOSTR_RELAY_URLS PSY_NOSTR_LOOKBACK_SECONDS
+  export PSY_NOSTR_ENABLED PSY_NOSTR_RELAY_URL PSY_NOSTR_RELAY_URLS PSY_NOSTR_LOOKBACK_SECONDS
 }
